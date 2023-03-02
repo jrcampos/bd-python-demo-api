c5b37963cc596207d19def13541595d0c61f9bae
João R. Campos <jrcampos@dei.uc.pt>
2023-03-02 10:00:24 +0000

    Databases Course Python Demo API

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# bd-python-demo-api
-Simple Databases Course Python Demo API
+# Databases - Practical Assignment
+
+The code and resources available in this repository are to be used only within the scope of the _BD 2023_ course of the Bachelor in Informatics Engineering.
+
+The main purpose of this repository is to provide examples on how to do the initial setup of the database-centric REST API that must be developed for the assignment.
+
+The system must be made available through a REST API that allows the user to access the system using HTTP requests (when content is required, JSON must be used). The followingfigure represents a simplified view of the system to be developed. As it is possible to see, the user interacts with the web server through the exchange of REST request/response and in turn the web server interacts with the database server through an SQL interface (e.g., Psycopg in the case of Python).
+
+<p align="center">
+  <img src="rest_api.png" />
+</p>
+
+_The contents of this repository do not replace the proper reading of the assignment description._
+
+## Demo [Python](python) REST API
+
+To start this demo run the script [`python demo-api.py`](demo-api.py). This will launch a local web server with the coded endpoints. You can then make requests to the endpoints through HTTP (e.g., open your web browser and access http://localhost:8080/departments). To organize the interactions with the web server it is best to use an application; for this assignment you must use [`Postman`](https://www.postman.com/downloads/). Postman supports _collections_, which allows you to group requests (such as those that you will have to develop for the practical assignment). You can also import collections (such as the examples provided).
+
+HTTP works as a request-response protocol. For this work, three main methods might be necessary:
+
+- **GET**: used to request data from a resource
+- **POST**: used to send data to create a resource
+- **PUT**: used to send data to update a resource
+
+In Postman you need to specify the type of the request when creating a new one. For POST/PUT requests, the data should be sent in the _body_ of the request, using the _raw_ format with _JSON_ as highlighted in the following screenshot. An example can also be found in the demo Postman collection made available.
+
+<p align="center">
+  <img src="postman_post.png" />
+</p>
+
+For most of the endpoints it will also be necessary to pass an authentication token. You can define the token for each request in either the _Authorization_ or _Headers_ tab in Postman (which can also be seen in the previous image). 
+
+The REST API must be developed to fulfil the functionalities/endpoints required for the practical assignment. You must also develop the database to support that application, which must be created in the PostgreSQL database that the web server connects to.
+
+## Overview of the Contents
+- [`python`](python) - Source code of web application template in python. It has template endpoints for the different types of requests (i.e., GET, POST, PUT) and how to interact with a PostgreSQL database server. This can/should be used as basis for the endpoints required for the practical assignment.
+- [`postman`](postman) - An example of a collection of requests exported from the Postman tool. This collection is to be imported in the [Postman application](https://www.postman.com/downloads/).
+
+
+## Requirements
+
+To execute this project it is required to have installed:
+
+- `python 3.X`
+  - `psycopg2`
+  - `flask`
+
+## Support
+
+If you find an issue or have questions regarding the demo feel free to contact me: [jrcampos@dei.uc.pt](mailto:jrcampos@dei.uc.pt)
+
+
+## Authors
+
+* BD 2023 Team - https://dei.uc.pt/lei/
+* University of Coimbra
---
+++ python/demo-api.py
@@ -0,0 +1,268 @@
+##
+## =============================================
+## ============== Bases de Dados ===============
+## ============== LEI  2022/2023 ===============
+## =============================================
+## =================== Demo ====================
+## =============================================
+## =============================================
+## === Department of Informatics Engineering ===
+## =========== University of Coimbra ===========
+## =============================================
+##
+## Authors:
+##   João R. Campos <jrcampos@dei.uc.pt>
+##   Nuno Antunes <nmsa@dei.uc.pt>
+##   BD 2022 Team - https://dei.uc.pt/lei/
+##   University of Coimbra
+
+
+import flask
+import logging
+import psycopg2
+import time
+
+app = flask.Flask(__name__)
+
+StatusCodes = {
+    'success': 200,
+    'api_error': 400,
+    'internal_error': 500
+}
+
+##########################################################
+## DATABASE ACCESS
+##########################################################
+
+def db_connection():
+    db = psycopg2.connect(
+        user='aulaspl',
+        password='aulaspl',
+        host='127.0.0.1',
+        port='5432',
+        database='dbfichas'
+    )
+
+    return db
+
+
+
+
+##########################################################
+## ENDPOINTS
+##########################################################
+
+
+@app.route('/')
+def landing_page():
+    return """
+
+    Hello World (Python Native)!  <br/>
+    <br/>
+    Check the sources for instructions on how to use the endpoints!<br/>
+    <br/>
+    BD 2022 Team<br/>
+    <br/>
+    """
+
+##
+## Demo GET
+##
+## Obtain all departments in JSON format
+##
+## To use it, access:
+##
+## http://localhost:8080/departments/
+##
+
+@app.route('/departments/', methods=['GET'])
+def get_all_departments():
+    logger.info('GET /departments')
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT ndep, nome, local FROM dep')
+        rows = cur.fetchall()
+
+        logger.debug('GET /departments - parse')
+        Results = []
+        for row in rows:
+            logger.debug(row)
+            content = {'ndep': int(row[0]), 'nome': row[1], 'localidade': row[2]}
+            Results.append(content)  # appending to the payload to be returned
+
+        response = {'status': StatusCodes['success'], 'results': Results}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /departments - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+##
+## Demo GET
+##
+## Obtain department with ndep <ndep>
+##
+## To use it, access:
+##
+## http://localhost:8080/departments/10
+##
+
+@app.route('/departments/<ndep>/', methods=['GET'])
+def get_department(ndep):
+    logger.info('GET /departments/<ndep>')
+
+    logger.debug(f'ndep: {ndep}')
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT ndep, nome, local FROM dep where ndep = %s', (ndep,))
+        rows = cur.fetchall()
+
+        row = rows[0]
+
+        logger.debug('GET /departments/<ndep> - parse')
+        logger.debug(row)
+        content = {'ndep': int(row[0]), 'nome': row[1], 'localidade': row[2]}
+
+        response = {'status': StatusCodes['success'], 'results': content}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /departments/<ndep> - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+##
+## Demo POST
+##
+## Add a new department in a JSON payload
+##
+## To use it, you need to use postman or curl:
+##
+## curl -X POST http://localhost:8080/departments/ -H 'Content-Type: application/json' -d '{'localidade': 'Polo II', 'ndep': 69, 'nome': 'Seguranca'}'
+##
+
+@app.route('/departments/', methods=['POST'])
+def add_departments():
+    logger.info('POST /departments')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logger.debug(f'POST /departments - payload: {payload}')
+
+    # do not forget to validate every argument, e.g.,:
+    if 'ndep' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'ndep value not in payload'}
+        return flask.jsonify(response)
+
+    # parameterized queries, good for security and performance
+    statement = 'INSERT INTO dep (ndep, nome, local) VALUES (%s, %s, %s)'
+    values = (payload['ndep'], payload['localidade'], payload['nome'])
+
+    try:
+        cur.execute(statement, values)
+
+        # commit the transaction
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted dep {payload["ndep"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'POST /departments - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+##
+## Demo PUT
+##
+## Update a department based on a JSON payload
+##
+## To use it, you need to use postman or curl:
+##
+## curl -X PUT http://localhost:8080/departments/ -H 'Content-Type: application/json' -d '{'ndep': 69, 'localidade': 'Porto'}'
+##
+
+@app.route('/departments/<ndep>', methods=['PUT'])
+def update_departments(ndep):
+    logger.info('PUT /departments/<ndep>')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logger.debug(f'PUT /departments/<ndep> - payload: {payload}')
+
+    # do not forget to validate every argument, e.g.,:
+    if 'localidade' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'localidade is required to update'}
+        return flask.jsonify(response)
+
+    # parameterized queries, good for security and performance
+    statement = 'UPDATE dep SET local = %s WHERE ndep = %s'
+    values = (payload['localidade'], ndep)
+
+    try:
+        res = cur.execute(statement, values)
+        response = {'status': StatusCodes['success'], 'results': f'Updated: {cur.rowcount}'}
+
+        # commit the transaction
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(error)
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+if __name__ == '__main__':
+
+    # set up logging
+    logging.basicConfig(filename='log_file.log')
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    host = '127.0.0.1'
+    port = 8080
+    app.run(host=host, debug=True, threaded=True, port=port)
+    logger.info(f'API v1.0 online: http://{host}:{port}')
